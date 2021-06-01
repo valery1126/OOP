d03ac7860967a16e4395b813ae300a56316ec3ec
valery1126 <valerymvc26@gmail.com>
2021-05-31 19:46:23 -0600

    Trabajo en Clase #6

---
+++ src/com/ucreativa/vacunacion/BitacoraVacunas.java
@@ -0,0 +1,29 @@
+package com.ucreativa.vacunacion;
+
+import java.util.Date;
+
+public class BitacoraVacunas {
+
+    private Persona persona;
+    private String marca;
+    private Date fecha;
+
+
+    public BitacoraVacunas(Persona persona, String marca, Date fecha) {
+        this.persona = persona;
+        this.marca = marca;
+        this.fecha = fecha;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+}
